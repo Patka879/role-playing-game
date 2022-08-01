bb926dbf3939572c5e8382a8fd84e03b2319b31b
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-08-01 23:51:59 +0200

    diceCount added to cards

--- index.js
+++ index.js
@@ -4,26 +4,35 @@ const hero = {
     elementId: "hero",
     name: "Wizard",
     avatar: "images/wizard.jpg",
-    health: "60",
-    diceRoll: "6"
+    health: 60,
+    diceRoll: [3, 1, 4],
+    diceCount: 3
 }
 
 const monster = {
     elementId: "monster",
     name: "Orc",
     avatar: "images/orc.png",
-    health: "10",
-    diceRoll: 4
+    health: 10,
+    diceRoll: [2],
+    diceCount: 1
 }
 
 function renderCharacter(data) {
-    const {elementId, name, avatar, health, diceRoll} = data
+    const {elementId, name, avatar, health, diceRoll, diceCount} = data
+
+    let diceHtml = ""
+
+    for (let i = 0; i < diceRoll.length; i++) {
+        diceHtml += `<div class="dice">${diceRoll[i]}</div>`
+    }
+
     document.getElementById(elementId).innerHTML = `
     <div class="character-card">
         <h4 class="name"> ${name} </h4>
         <img class="avatar" src=${avatar}>
         <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
+        <div class="dice-container"> ${diceHtml}</div>
     </div> `
 }
 
