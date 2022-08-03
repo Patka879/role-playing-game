a9102e55d9ce7ac20647bbe59ff9b5afcd4ca1ea
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-08-03 22:08:18 +0200

    rendering function changed into constructor method

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 "use strict"
 
+function getDiceRollArray(diceCount) {
+    return new Array(diceCount).fill(0).map(function(){
+        return Math.floor(Math.random() * 6) + 1
+    })        
+}
+
+function getDiceHtml(diceCount) {
+    return getDiceRollArray(diceCount).map(function(num){ 
+        return  `<div class="dice">${num}</div>`
+    }).join('')
+}
+
 const hero = {
     elementId: "hero",
     name: "Wizard",
-    avatar: "images/wizard.jpg",
+    avatar: "images/wizard.png",
     health: 60,
-    diceRoll: [3, 1, 4],
     diceCount: 3
 }
 
@@ -14,25 +25,33 @@ const monster = {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: [2],
     diceCount: 1
 }
 
-function renderCharacter(data) {
-    const {elementId, name, avatar, health, diceRoll, diceCount} = data
+function Character(data) {
+    this.elementId = data.elementId
+    this.name = data.name
+    this.avatar = data.avatar
+    this.health = data.health
+    this.diceCount = data.diceCount
+    this.getCharacterHtml = function() {
+        const {elementId, name, avatar, health, diceCount} = this
+        const diceHtml = getDiceHtml(diceCount)
 
-    let diceHtml = diceRoll.map(function(roll) {
-        return `<div class="dice">${roll}</div>`
-    })
+        document.getElementById(elementId).innerHTML =
+            `<div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                <div class="dice-container">    
+                    ${diceHtml}
+                </div>
+            </div>`
+        }
+    }
 
-    document.getElementById(elementId).innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src=${avatar}>
-        <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container"> ${diceHtml.join()}</div>
-    </div> `
-}
+const wizard = new Character(hero)
+const orc = new Character(monster)
 
-renderCharacter(hero)
-renderCharacter(monster)
+wizard.getCharacterHtml()
+orc.getCharacterHtml()
